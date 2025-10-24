8605872da699d5164a58edfbe3a8bb138ea25128
nobbydoo80 <cstewart13@art.edu>
2025-10-24 07:11:28 -0700

    Update event webhook

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,20 @@
 module.exports = function(eleventyConfig) {
-  // Copy assets and videos into _site
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/videos": "videos" });
+  // Copy assets and videos into _site
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/videos": "videos" });
+    
+    // 🔥 FIX: ADD THIS LINE TO COPY THE IMAGES DIRECTORY
+    eleventyConfig.addPassthroughCopy({ "src/images": "images" });
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_layouts",
-      data: "_data",
-    },
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-  };
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
 };
